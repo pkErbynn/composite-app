38935a7012ac49515188c959f947cc891f6e8d2b
pkerbynn <john.erbynn@gmail.com>
2020-06-15 07:12:40 +0000

    add bg color'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { App1SharedModule } from "projects/app1/src/app/app.module";
 import { App2SharedModule } from "projects/app2/src/app/app.module";
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
